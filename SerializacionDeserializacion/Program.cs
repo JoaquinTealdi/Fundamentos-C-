d67494583f5fb6916a2854b9aab6ca97a2cde438
Joaquin Tealdi <joaquintealdi25@gmail.com>
2023-11-06 18:08:44 -0300

    add serialization and deserialization

---
+++ SerializacionDeserializacion/Program.cs
@@ -0,0 +1,17 @@
+﻿using models;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //var bebida = new Cerveza("rubia", 15, "Heineken");
+        //string miJoson = JsonSerializer.Serialize(bebida);
+        //File.WriteAllText("nombreArchivo.txt", miJoson); //se guarda en carpeta 'Debug'
+
+        string miJson1 = File.ReadAllText("nombreArchivo.txt"); 
+        var cerveza = JsonSerializer.Deserialize<Cerveza>(miJson1);
+
+        Console.WriteLine($"Objeto deserializado! \n{cerveza}");
+    }
+}
